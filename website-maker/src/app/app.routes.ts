17a5b6b77af3f7890baf6d94ec290b05ed05f864
ArpitSharma0713 <153850419+ArpitSharma0713@users.noreply.github.com>
2025-08-18 16:17:38 +0530

    adding

--- website-maker/src/app/app.routes.ts
+++ website-maker/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Pg1 } from './pgs/pg1/pg1';
 import { Pg2 } from './pgs/pg2/pg2';
 import { Pg3 } from './pgs/pg3/pg3';
+import { Pg4 } from './pgs/pg4/pg4';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: Pg1 },
   { path: 'register', component: Pg3 },
   { path: 'forgot-password', component: Pg2 },
-  { path: '**', redirectTo: '/login' }
+  { path: '**', redirectTo: '/login' },
+  { path: 'Home', component: Pg4 }
 ];
--- website-maker/src/app/components/pg4-header/pg4-header.ts
+++ website-maker/src/app/components/pg4-header/pg4-header.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pg4-header',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './pg4-header.html',
   styleUrl: './pg4-header.css'
 })
 export class Pg4Header {
+  constructor(private router: Router) {}
+  onLinkClick(link: string) {
+    if(link=="Link 1") {
+      this.router.navigate(['/login']);
+    } else if(link=="Link 2") {
+      this.router.navigate(['/register']);
+    } else if(link=="Link 3") {
+      this.router.navigate(['/Home']);
+    }
+  }
 
 }
